f42b265279c73352fcda0f048db3ad0050fd677d
janjunyoung123 <hjy961017@gmail.com>
2024-07-24 15:13:41 +0900

    포리치

---
+++ js/forEach.js
@@ -0,0 +1,6 @@
+window.addEventListener("load",function(){
+    const menu = this.document.querySelectorAll(".submenu > li")
+    menu.forEach(function(menus){
+        console.log(menus);
+    })
+})
